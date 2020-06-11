e6b6ee4dae7546654e1e3112939cf0a7f0d2ad45
Oluwasayo <oluwasayo12@gmail.com>
2020-06-11 22:12:55 +0100

    minor change to test folder

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingWorldMessage() {
     String someone = "World, Thanks you making me Very Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
